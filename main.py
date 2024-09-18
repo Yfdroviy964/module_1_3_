fdcc954ee65819cc651315f9cd7cea3d16b9e788
Yfdroviy964 <maccadg@mail.ru>
2024-09-18 17:23:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+Name ='Андрей'
+print('Здравствуйте! Меня зовут ', Name)
+print(type(Name))
+Name = 'октябре'
+Age = int(56)
+print('Мой Возраст',Age, 'лет')
+print(type(Age))
+Age =  Age+1
+print('В',Name, 'мне будет', Age, 'лет' )
+Age = int(Age)
+print(type(Age))
+Is_Student = True
+print('Я студент URBAN - universiti')
+print(type(Is_Student))
